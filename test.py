e4ec06dcc5c54b453051f97b19a41ea3dad27fca
Gurupra5ad <guruprasad2511@gmail.com>
2021-02-19 15:37:39 +0530

    Finished code and deleted the test codes

--- test.py
+++
@@ -1,8 +0,0 @@
-num = 89
-if num<=9:
-        print('The number is between 1 to 9')
-else:
-    str_num = str(num)
-    str_num1 = str_num[0]
-    str_print = 'The number is between ' + str_num1 + '1 to ' + str_num1 + '9'
-    print(str_print)
